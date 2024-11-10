45e1ff2512855e435d57fb18ba2f13b90cd6e575
niki2740 <147843584+niki2740@users.noreply.github.com>
2024-11-10 03:47:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-# tests/test_my_package
-from hello import hello
+from hello import hello, goodbye
 
 def test_hello():
     assert hello("World") == "Hello, World!"
+
+def test_goodbye():
+    assert goodbye("Python") == "До свидания, Python!"
